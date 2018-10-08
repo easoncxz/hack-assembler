74aa8b3713dc02bd70591959fb9fd78f0b563327
Eason Chen <easoncxz@gmail.com>
2018-10-08 19:23:44 +1300

    Jump through hoops to get Ruby 2.5.1 via RVM

--- automation/travis-install.sh
+++ automation/travis-install.sh
@@ -3,14 +3,29 @@
 set -e  # Fail fast
 set -x  # Be noisy
 
+# Bring Homebrew up to date, ...
+brew doctor || true
+brew update || true
+
+# ... so that we can install GnuPG, ...
+brew install gnupg || brew upgrade gnupg || which gpg2
+gpg2 --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+# ... so that we can verify the new RVM binary when we download it, ...
 rvm get latest
-rvm install 'ruby-2.5.1'    # homebrew_automation depends on certain Ruby versions
+
+# ... so that we can have a version of Ruby that works with homebrew_automation, ...
+rvm osx-ssl-certs update all    # https://github.com/rubygems/rubygems.org/issues/613
+rvm install 'ruby-2.5.1'
+
+# ... so that we can install homebrew_automation.
+gem update --system   # https://stackoverflow.com/questions/10246023/
 gem install homebrew_automation -v 0.0.8
 homebrew_automation.rb help
 
-brew doctor || true
-brew update || true
+# And then install our Haskell build tool.
 which stack || brew install --verbose haskell-stack
 
+# Download a version of GHC and compile all our Haskell dependencies. This takes ages.
 stack --no-terminal setup
 stack --no-terminal test --only-dependencies
