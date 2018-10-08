11424d2d5f97a0c8f253fad1939e25e33fb70757
Eason Chen <easoncxz@gmail.com>
2018-10-08 19:12:06 +1300

    Specify Ruby version to use

--- automation/travis-install.sh
+++ automation/travis-install.sh
@@ -3,6 +3,8 @@
 set -e  # Fail fast
 set -x  # Be noisy
 
+rvm get latest
+rvm install 'ruby-2.5.1'    # homebrew_automation depends on certain Ruby versions
 gem install homebrew_automation -v 0.0.8
 homebrew_automation.rb help
 
