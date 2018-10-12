c0589661bac6faf68bed67d9a7053f4b369f7118
Eason Chen <easoncxz@gmail.com>
2018-10-12 23:57:11 +1300

    Move script lines into own file (WIP)

---
+++ automation/install-stack.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Install our Haskell build tool.
+which -a stack || ( curl -sSL https://get.haskellstack.org/ | sh )
+
+# Download a version of GHC and compile all our Haskell dependencies. This takes ages.
+stack --no-terminal setup
+stack --no-terminal test --only-dependencies
