5ab1623edfb71b52f311a24d4fc984975b7a3ef1
Eason Chen <easoncxz@gmail.com>
2018-10-13 00:02:14 +1300

    Try out new build setup

--- automation/install-homebrew-automation.sh
+++ automation/install-homebrew-automation.sh
@@ -1,41 +1,43 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash --login
 
 set -e  # Fail fast
 set -x  # Be noisy
 
 # Bring Homebrew up to date, ...
-#brew doctor || true
-#brew update || true
+brew --version
+brew doctor || true
+brew upgrade || true
+brew --version
 
 # ... so that we can install GnuPG, ...
-#brew install gnupg || brew upgrade gnupg || which gpg
-#echo 'Expecting we have GnuPG v2.x'
-#gpg --version
-#gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+which -a gpg || brew install gnupg
+echo 'Expecting to have GnuPG v2.x'
+gpg --version
+gpg --keyserver hkp://pgp.mit.edu --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
+set +x  # RVM is too noisy...
+function with_echo {
+    echo "$*";
+    # Crucially, without quotes:
+    $*
+}
 
 # ... so that we can verify the new RVM binary when we download it, ...
-#rvm get latest
+with_echo rvm version
+with_echo rvm get latest
+with_echo rvm version
+with_echo rvm disk-usage all
 
 # ... so that we can have a version of Ruby that works with homebrew_automation, ...
-#rvm osx-ssl-certs update all    # https://github.com/rubygems/rubygems.org/issues/613
-#rvm install 'ruby-2.5.1'
-#rvm use 'ruby-2.5.1'
+with_echo cat .ruby-version
+with_echo rvm use "$(cat .ruby-version)" --install
+with_echo which -a ruby
+with_echo which -a gem
+with_echo ruby --version
+with_echo gem --version
 
-which -a ruby
-which -a gem
-ruby --version
-gem --version
+set -x
 
 # ... so that we can install homebrew_automation.
-#gem update --system   # https://stackoverflow.com/questions/10246023/
 gem install homebrew_automation -v 0.0.8
 homebrew_automation.rb help
-
-# And then install our Haskell build tool.
-brew doctor || true
-brew update || true
-which stack || brew install --verbose haskell-stack
-
-# Download a version of GHC and compile all our Haskell dependencies. This takes ages.
-stack --no-terminal setup
-stack --no-terminal test --only-dependencies
