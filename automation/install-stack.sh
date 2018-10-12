4d61c37d77b9a0af3bade9258b70ffb8c28ab9bc
Eason Chen <easoncxz@gmail.com>
2018-10-13 00:49:55 +1300

    Remember to `rvm reload` in outer shell

--- automation/install-stack.sh
+++ automation/install-stack.sh
@@ -6,3 +6,7 @@ which -a stack || ( curl -sSL https://get.haskellstack.org/ | sh )
 # Download a version of GHC and compile all our Haskell dependencies. This takes ages.
 stack --no-terminal setup
 stack --no-terminal test --only-dependencies
+
+# Get a rough idea of Travis cache size
+du -h -d 3 ~/.stack
+du -h -d 3 .stack-work
