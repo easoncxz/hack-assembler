f4b12d3e450dded322cc500713337309dc7fb8c1
Eason Chen <easoncxz@gmail.com>
2019-08-22 00:18:52 +1200

    Debug Travis deployment setup (bump version)

--- automation/gather-and-publish.sh
+++ automation/gather-and-publish.sh
@@ -3,14 +3,16 @@
 set -e
 set -x
 
-homebrew_automation.rb bottle gather-and-publish \
-    --source-user easoncxz \
-    --source-repo hack-assembler \
-    --source-tag "$(stack exec -- hack-assembler-automation version-as-tag)" \
-    \
-    --tap-user easoncxz \
-    --tap-repo homebrew-tap \
-    --tap-token "$EASONCXZ_GITHUB_OAUTH_TOKEN_v2" \
-    \
-    --bintray-user easoncxz \
-    --bintray-token "$EASONCXZ_BINTRAY_API_KEY"
+echo 'call homebrew_automation.rb bottle gather-and-publish'
+
+# homebrew_automation.rb bottle gather-and-publish \
+#     --source-user easoncxz \
+#     --source-repo hack-assembler \
+#     --source-tag "$(stack exec -- hack-assembler-automation version-as-tag)" \
+#     \
+#     --tap-user easoncxz \
+#     --tap-repo homebrew-tap \
+#     --tap-token "$EASONCXZ_GITHUB_OAUTH_TOKEN_v2" \
+#     \
+#     --bintray-user easoncxz \
+#     --bintray-token "$EASONCXZ_BINTRAY_API_KEY"
