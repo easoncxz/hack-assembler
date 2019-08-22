e38f390efaf25b75de998b1d7b18e26a640bb288
Eason Chen <easoncxz@gmail.com>
2019-08-22 23:45:32 +1200

    Do more bash, less dot-slash
    For unknownable reasons, since as early as 2016, Travis's "script"
    deployment provider has not supported a simple dot-slash call inside the
    `script` block of a `deploy` section. It appears either a `chmod +x a.sh
    && a.sh` or `bash a.sh` is needed.
    
    See old discussions on Github issues:
    
    - <https://github.com/travis-ci/travis-ci/issues/5538>
    - <https://github.com/travis-ci/travis-ci/issues/8867>
    
    In this commit, I re-activate all actual logic, and switch all
    deployment definitions to use the not-dot-slash approach. Hopefully this
    tagged commit does something good.

--- automation/bottle-and-upload.sh
+++ automation/bottle-and-upload.sh
@@ -3,16 +3,14 @@
 set -x
 set -e
 
-echo 'call homebrew_automation.rb bottle build-and-upload'
-
-# homebrew_automation.rb bottle build-and-upload \
-#     --source-user easoncxz \
-#     --source-repo hack-assembler \
-#     --source-tag "$(stack exec -- hack-assembler-automation version-as-tag)" \
-#     \
-#     --tap-user easoncxz \
-#     --tap-repo homebrew-tap \
-#     --tap-token "$EASONCXZ_GITHUB_OAUTH_TOKEN_v2" \
-#     \
-#     --bintray-user easoncxz \
-#     --bintray-token "$EASONCXZ_BINTRAY_API_KEY"
+homebrew_automation.rb bottle build-and-upload \
+    --source-user easoncxz \
+    --source-repo hack-assembler \
+    --source-tag "$(stack exec -- hack-assembler-automation version-as-tag)" \
+    \
+    --tap-user easoncxz \
+    --tap-repo homebrew-tap \
+    --tap-token "$EASONCXZ_GITHUB_OAUTH_TOKEN_v2" \
+    \
+    --bintray-user easoncxz \
+    --bintray-token "$EASONCXZ_BINTRAY_API_KEY"
